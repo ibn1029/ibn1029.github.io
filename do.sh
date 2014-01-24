1c3ff6a190bbdbd5cf0534b280e1c452c7fab023
viage <ibn1029@gmail.com>
2014-01-24 20:44:41 +0900

    update

---
+++ do.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ `hostname` == dti-vps-srv85 ]; then
+    cd /home/viage/work/ibn1029.github.io
+else
+    cd /Users/viage/Work/App/ibn1029.github.io
+fi
+carton exec perl get_archives.pl
